b45b161e54d92499b98fa8c12d6187b40f44a0a6
WebDeverDan <danielbvideo@gmail.com>
2022-12-30 13:53:13 -0500

    Updates

---
+++ precache-manifest.d61e3b935e8e70496d513738a325cbc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0edd43efcbe0e04a6e747b5b8ba771b8",
+    "url": "/shopping-budget-app/index.html"
+  },
+  {
+    "revision": "fce56ef2c6f27b8f1039",
+    "url": "/shopping-budget-app/static/css/main.63fa5418.chunk.css"
+  },
+  {
+    "revision": "adf0e6bbfdabc38eda41",
+    "url": "/shopping-budget-app/static/js/2.6554e649.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-budget-app/static/js/2.6554e649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce56ef2c6f27b8f1039",
+    "url": "/shopping-budget-app/static/js/main.a054b6a2.chunk.js"
+  },
+  {
+    "revision": "fec3c1c32df759ba0799",
+    "url": "/shopping-budget-app/static/js/runtime-main.4553f54d.js"
+  }
+]);
