fefbe48ee560b693a2e3c9e14093e960cb04d616
WebDeverDan <danielbvideo@gmail.com>
2023-01-01 12:04:06 -0500

    Updates

--- precache-manifest.3eb6cb8e8efed8e5331afe50bee193b4.js
+++ precache-manifest.3eb6cb8e8efed8e5331afe50bee193b4.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ad22adcb4d432b5b0de5ccfc6ddbd92",
+    "revision": "dff546776d82a244a872754043924045",
     "url": "/shopping-budget-app/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/shopping-budget-app/static/css/2.83e4c9b7.chunk.css"
   },
   {
-    "revision": "2aadfeaa7a756831ab8f",
-    "url": "/shopping-budget-app/static/css/main.87ce23f6.chunk.css"
+    "revision": "17a7a3bae88c7fdd14e4",
+    "url": "/shopping-budget-app/static/css/main.c07faa73.chunk.css"
   },
   {
     "revision": "d6b6c397fc2346157ce4",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/shopping-budget-app/static/js/2.b48ae878.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2aadfeaa7a756831ab8f",
-    "url": "/shopping-budget-app/static/js/main.728d963a.chunk.js"
+    "revision": "17a7a3bae88c7fdd14e4",
+    "url": "/shopping-budget-app/static/js/main.a91dc26d.chunk.js"
   },
   {
     "revision": "fec3c1c32df759ba0799",
