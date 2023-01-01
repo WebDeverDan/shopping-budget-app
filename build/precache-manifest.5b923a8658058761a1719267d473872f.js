b0111a39a334155dcc4ce5c6373ab3abee8257d2
WebDeverDan <danielbvideo@gmail.com>
2023-01-01 11:04:18 -0500

    added new favicon

---
+++ build/precache-manifest.5b923a8658058761a1719267d473872f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2882e76f8f5effb7bed1b8d2877d8022",
+    "url": "/shopping-budget-app/index.html"
+  },
+  {
+    "revision": "6b93bb87c978e350288f",
+    "url": "/shopping-budget-app/static/css/2.83e4c9b7.chunk.css"
+  },
+  {
+    "revision": "cc8190974eda1822f954",
+    "url": "/shopping-budget-app/static/css/main.912cc870.chunk.css"
+  },
+  {
+    "revision": "6b93bb87c978e350288f",
+    "url": "/shopping-budget-app/static/js/2.5d972221.chunk.js"
+  },
+  {
+    "revision": "9fd8ac69f8779f68b807eb06f9423c10",
+    "url": "/shopping-budget-app/static/js/2.5d972221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8190974eda1822f954",
+    "url": "/shopping-budget-app/static/js/main.1adbf2d9.chunk.js"
+  },
+  {
+    "revision": "fec3c1c32df759ba0799",
+    "url": "/shopping-budget-app/static/js/runtime-main.4553f54d.js"
+  }
+]);
